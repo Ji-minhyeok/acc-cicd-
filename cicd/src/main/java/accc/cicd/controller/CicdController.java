ffd806416ee2b4165a7d2737598369bba12f25a5
Ji-minhyeok <gmh8257@gmail.com>
2024-04-04 12:49:05 +0900

    Feat: controller 수정 12:48

--- cicd/src/main/java/accc/cicd/controller/CicdController.java
+++ cicd/src/main/java/accc/cicd/controller/CicdController.java
@@ -8,6 +8,6 @@ public class CicdController{
 	
 	@GetMapping("/hello")
 	public String helloWorld(){
-		return "Hello World! I changed something twice!";
+		return "Hello World! I changed something again!";
 	}
 }
